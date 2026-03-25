fd3c7614c3cf1524fd97e318ffe75144d564f69d
Pedro Fonseca <pedro.fonseca01@utrgv.edu>
2026-03-25 14:59:18 -0500

    last changes

--- hello.py
+++ hello.py
@@ -1,6 +1,3 @@
 print("Hello from the feature-python branch")
-<<<<<<< HEAD
 print("Hello from the main branch")
-=======
 print("Hello again from the feature-python branch")
->>>>>>> feature-python
